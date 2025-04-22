525f2629eb317af93c11a7b1bae71b2b77a7df5a
yadev07 <yadevsingh.nishad@gmail.com>
2025-04-22 10:45:35 +0530

    Updated

--- Tic_Tac_Toe/script.js
+++ Tic_Tac_Toe/script.js
@@ -7,106 +7,186 @@ let btn6 = document.getElementById("btn6");
 let btn7 = document.getElementById("btn7");
 let btn8 = document.getElementById("btn8");
 let btn9 = document.getElementById("btn9");
-let c1 = c2 = c3 = c4 = c5 = c6 = c7 = c8 = c9 = 0;
-chance = 'X';
+let res = document.getElementById("reset");
 
+let c1 = c2 = c3 = c4 = c5 = c6 = c7 = c8 = c9 = 0;
+let chance = 'O';
+
+res.onclick = function(){
+    reset();
+    document.getElementById('result').innerText = "";
+}
 
 btn1.onclick = function () {
-    if(c1==1) return;
+    if(checkWin()){
+        return;
+    }
+    if(c1!=0) return; c1++;
+    change_chance();
     btn1.innerText = chance;
     checkWin();
-    change_chance();
-    c1++;
+
+   
 };
 btn2.onclick = function () {
-    if(c2==1) return;
+    if(checkWin()){
+        return;
+    }
+    if(c2!=0) return; c2++;
+    change_chance();
     btn2.innerText = chance;
-    checkWin();
-    change_chance(); 
-    c2++;
+     checkWin();
+  
 };
 btn3.onclick = function () {
-    if(c3==1) return;
-    btn3.innerText = chance;
-    checkWin();
+    if(checkWin()){
+        return;
+    }
+    if(c3!=0) return; c3++;
     change_chance();
-    c3++;
+    btn3.innerText = chance;
+     checkWin();
+  
 };
 btn4.onclick = function () {
-    if(c4==1) return;
+    if(checkWin()){
+        return;
+    }
+    if(c4!=0) return; c4++;
+    change_chance();
     btn4.innerText = chance;
     checkWin();
-    change_chance();
-    c4++;
+
 };
 btn5.onclick = function () {
-    if(c5==1) return;
+    if(checkWin()){
+        return;
+    }
+    if(c5!=0) return;   c5++;
+    change_chance();
     btn5.innerText = chance;
     checkWin();
-    change_chance();
-    c5++;
+
 };
 btn6.onclick = function () {
-    if(c6==1) return;
+    if(checkWin()){
+        return;
+    }
+    if(c6!=0) return;    c6++;
+    change_chance();
     btn6.innerText = chance;
     checkWin();
-    change_chance();
-    c6++;
+ 
+
 };
 btn7.onclick = function () {
-    if(c7==1) return;
+    if(checkWin()){
+        return;
+    }
+    if(c7!=0) return; c7++;
+    change_chance();
     btn7.innerText = chance;
     checkWin();
-    change_chance();
-    c7++;
+  
+   
 };
 btn8.onclick = function () {
-    if(c8==1) return;
+    if(checkWin()){
+        return;
+    }
+    if(c8!=0) return;
+    c8++;
+    change_chance();
     btn8.innerText = chance;
     checkWin();
-    change_chance();
-    c8++;
+   
+    
 };
 btn9.onclick = function () {
-    if(c9==1) return;
-    btn9.innerText = chance;
-    checkWin();
-    change_chance();
+    if(checkWin()){
+        return;
+    }
+    if(c9!=0) return;
     c9++;
+    change_chance();
+    btn9.innerText = chance;
+     checkWin();
+    
 };
 
 
 function checkWin() {
     if(btn1.innerText == btn2.innerText && btn1.innerText == btn3.innerText && btn1.innerText != ""){
-        document.getElementById('result').innerText = chance + "Wins..!";
+        document.getElementById('result').innerText = chance + " Wins..!";
+        alert(chance + " Wins..!");
+        return true;
     }
     else if(btn4.innerText == btn5.innerText && btn4.innerText == btn6.innerText && btn4.innerText != ""){
-        document.getElementById('result').innerText = chance + "Wins..!";
+        document.getElementById('result').innerText = chance + " Wins..!";
+        alert(chance + " Wins..!");
+        return true;
     }    
-    else if(btn7.innerText == btn8.innerText && btn7.innerText == bt9.innerText && btn9.innerText != ""){
-        document.getElementById('result').innerText = chance + "Wins..!";
+    else if(btn7.innerText == btn8.innerText && btn7.innerText == btn9.innerText && btn7.innerText != ""){
+        document.getElementById('result').innerText = chance + " Wins..!";
+        alert(chance + " Wins..!");
+        return true;
     }
-    else if(btn1.innerText == btn4.innerText && btn1.innerText == btn7.innerText && btn7.innerText != ""){
-        document.getElementById('result').innerText = chance + "Wins..!";
+    else if(btn1.innerText == btn4.innerText && btn1.innerText == btn7.innerText && btn1.innerText != ""){
+        document.getElementById('result').innerText = chance + " Wins..!";
+        alert(chance + " Wins..!");
+        return true;
     }
-    else if(btn2.innerText == btn5.innerText && btn2.innerText == btn8.innerText && btn8.innerText != ""){
-        document.getElementById('result').innerText = chance + "Wins..!";
+    else if(btn2.innerText == btn5.innerText && btn2.innerText == btn8.innerText && btn2.innerText != ""){
+        document.getElementById('result').innerText = chance + " Wins..!";
+        alert(chance + " Wins..!");
+        return true;
     }
-    else if(btn3.innerText == btn6.innerText && btn3.innerText == btn9.innerText && btn9.innerText != ""){
-        document.getElementById('result').innerText = chance + "Wins..!";
+    else if(btn3.innerText == btn6.innerText && btn3.innerText == btn9.innerText && btn3.innerText != ""){
+        document.getElementById('result').innerText = chance + " Wins..!";
+        alert(chance + " Wins..!");
+        return true;
     }
-    else if(btn1.innerText == btn5.innerText && btn1.innerText == btn9.innerText && btn9.innerText != ""){
-        document.getElementById('result').innerText = chance + "Wins..!";
+    else if(btn1.innerText == btn5.innerText && btn1.innerText == btn9.innerText && btn1.innerText != ""){
+        document.getElementById('result').innerText = chance + " Wins..!";
+        alert(chance + " Wins..!");
+        return true;
     }
-    else if(btn3.innerText == btn5.innerText && btn3.innerText == btn7.innerText && btn7.innerText != ""){
-        document.getElementById('result').innerText = chance + "Wins..!";
+    else if(btn3.innerText == btn5.innerText && btn3.innerText == btn7.innerText && btn3.innerText != ""){
+        document.getElementById('result').innerText = chance + " Wins..!";
+        alert(chance + " Wins..!");
+        return true;
     }
-    
+    return false; 
 }
-function change_chance() {
+
+change_chance = function() {
     if(chance == 'X'){
         chance = 'O';
     }else if(chance == 'O'){
         chance = 'X';
     }
+}
+
+reset = function(){
+    chance = "O";
+    c1 = c2 = c3 = c4 = c5 = c6 = c7 = c8 = c9 = 0;
+    btn1.innerText = "";
+    btn2.innerText = "";
+    btn3.innerText = "";
+    btn4.innerText = "";
+    btn5.innerText = "";
+    btn6.innerText = "";
+    btn7.innerText = "";
+    btn8.innerText = "";
+    btn9.innerText = "";
+}
+
+isFull = function(){
+    let a = document.getElementsByClassName('box');
+    for(let i=0 ; i,9 ; i++){
+        if(a[i].innerText == ""){
+            return false;
+        }
+    }
+    return true;
 }
